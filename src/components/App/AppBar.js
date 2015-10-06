4d41584726c310552e305dae3bb72c437f9b4019
Mark Hintz <mark@interactivethings.com>
2015-10-06 17:53:57 +0200

    Change the order of importance of the logo and the title in the AppBar component

--- src/components/App/AppBar.js
+++ src/components/App/AppBar.js
@@ -16,12 +16,8 @@ function style(theme) {
       textTransform: 'uppercase'
     },
     img: {
-      ...line(theme, {level: 2}),
       display: 'inline-block',
-      float: 'left',
-      height: '1em',
-      margin: `0 ${theme.sizeS}px 0 0`,
-      width: '1em'
+      width: '100%'
     }
   };
 }
@@ -32,12 +28,13 @@ class AppBar extends React.Component {
     theme: PropTypes.object.isRequired,
     logoSrc: PropTypes.string
   }
+
   render() {
-    const { title } = this.props;
+    const { title, logoSrc } = this.props;
     let currentStyle = style(this.props.theme);
     return (
       <div style={currentStyle.bar}>
-        <h1 style={currentStyle.h1}>{title}{this.props.logoSrc && <img style={currentStyle.img} src={this.props.logoSrc} />}</h1>
+        <h1 style={currentStyle.h1}>{logoSrc ? <img style={currentStyle.img} src={logoSrc} /> : (title ? title : "")}</h1>
       </div>
     );
   }
