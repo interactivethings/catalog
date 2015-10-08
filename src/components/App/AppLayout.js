9647427a6c985996e1a6917ae23a8cf922bbf898
Jeremy Stucki <jeremy@interactivethings.com>
2015-10-08 17:55:40 +0200

    Fix layout

--- src/components/App/AppLayout.js
+++ src/components/App/AppLayout.js
@@ -22,6 +22,8 @@ function style(
       background: theme.background,
       margin: 0,
       padding: 0,
+      width: '100%',
+      height: '100%',
       position: 'relative'
     },
     menuIcon: {
