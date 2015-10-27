e29fefaff4e10558fed3ce517c9184bb72b6d2f3
Gerhard Bliedung <gerhard@interactivethings.com>
2015-10-27 10:36:56 +0100

    decaf Loader

---
+++ src/components/Page/Loader.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Radium from 'radium';
+
+const SHOW_AFTER_MS = 500;
+
+const loaderKeyframes = Radium.keyframes({
+  '0%': {transform: 'rotate(0deg)' },
+  '50%': {transform: 'rotate(180deg)' },
+  '100%': {transform: 'rotate(360deg)' },
+}, 'Loader');
+
+const styles = {
+  spinner: {
+    borderColor: '#EEEEEE #D3D3D3 #B6B6B6 #999999',
+    borderRadius: '50px',
+    borderStyle: 'solid',
+    borderWidth: '3px',
+    height: '50px',
+    margin: 'calc(50% - 25px) auto 0 auto',
+    width: '50px',
+    animation: `${loaderKeyframes} 2s linear infinite`,
+  },
+  hidden: {
+    display: 'none',
+  },
+}
+
+class Loader extends React.Component {
+  state = {
+    visible: false
+  }
+
+  componentDidMount(){
+    this.interval = setTimeout( () => this.setState({visible: true}), SHOW_AFTER_MS);
+  }
+
+  componentWillUnmount(){
+    if (this.interval) {
+      clearTimeout(this.interval);
+    }
+  }
+
+  render() {
+    const {theme} = this.props;
+
+    let loader = this.state.visible
+      ? styles.spinner
+      : styles.hidden;
+
+    return <div style={loader} className='cg-Page-Loader'/>
+  }
+}
+
+export default Radium(Loader);
