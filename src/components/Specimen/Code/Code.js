af865b7eea340fd694eaf6dd4658865cd5418d10
Gerhard Bliedung <gerhard@interactivethings.com>
2015-10-27 10:37:11 +0100

    decaf Code

---
+++ src/components/Specimen/Code/Code.js
@@ -0,0 +1,39 @@
+import React, { PropTypes } from 'react';
+
+function getStyle(theme) {
+  return {
+    container: {
+      border: 'none',
+      borderRadius: '2px',
+      boxSizing: 'border-box',
+      display: 'block',
+      margin: '10px 0',
+      padding: '20px',
+      width: '100%',
+      height: 'auto',
+
+      background: '#fff',
+      border: '1px solid #eee',
+      color: theme.textColor,
+      fontFamily: theme.fontMono,
+      fontSize: theme.fontXs,
+      lineHeight: 1.4,
+      overflowX: 'scroll',
+      whiteSpace: 'pre',
+    }
+  }
+}
+
+class Code extends React.Component {
+  static propTypes = {
+    body: PropTypes.string.isRequired,
+    theme: PropTypes.object.isRequired,
+  }
+  render() {
+    const {theme, body} = this.props;
+    let styles = getStyle(theme);
+    return <section style={styles.container} dangerouslySetInnerHTML={{__html: body}}/>
+  }
+}
+
+export default Code;
