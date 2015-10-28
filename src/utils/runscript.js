5f01354435b46c6b8735011aa0d331d9174fa1f2
Gerhard Bliedung <gerhard@interactivethings.com>
2015-10-28 16:38:21 +0100

    decaf runscript

---
+++ src/utils/runscript.js
@@ -0,0 +1,64 @@
+//
+// Sequentially runs scripts as they are added
+//
+
+module.exports = () => {
+  let current = null;
+  let queue = [];
+  let enqueue = (handler) => {
+    if (current != null) {
+      return queue.push(handler);
+    } else {
+      return dequeue(handler);
+    }
+  };
+  let dequeue = (handler) => {
+    current = handler();
+    current.then( () => {
+      current = null;
+      if (queue.length > 0) {
+        return dequeue(queue.shift());
+      }
+    });
+    return current["catch"]( () => {
+      return console.error('Error loading script');
+    });
+  };
+  let execRemote = (src) => {
+    return () => {
+      return new Promise( (resolve, reject) => {
+        return execScript( (script) => {
+          script.addEventListener('load', resolve, false);
+          script.addEventListener('error', reject, false);
+          return script.setAttribute('src', src);
+        });
+      });
+    };
+  };
+  let execInline = (src) => {
+    return () => {
+      return new Promise( (resolve, reject) => {
+        return execScript( (script) => {
+          script.appendChild(document.createTextNode(src));
+          return resolve();
+        });
+      });
+    };
+  };
+  let execScript = (decorate) => {
+    var head, script;
+    script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    decorate(script);
+    head = document.getElementsByTagName('head')[0] || document.documentElement;
+    return head.appendChild(script);
+  };
+  return (srcOrEl) => {
+    if (_.isString(srcOrEl) && !_.isEmpty(srcOrEl.trim())) {
+      enqueue(execRemote(srcOrEl));
+    }
+    if (srcOrEl.textContent && !_.isEmpty(srcOrEl.textContent.trim())) {
+      return enqueue(execInline(srcOrEl.textContent));
+    }
+  };
+};
