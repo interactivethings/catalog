3449126dfcfe7b3a2288a8be982304b94701337d
Gerhard Bliedung <gerhard@interactivethings.com>
2015-11-05 16:27:39 +0100

    the color component now supports palettes

--- src/components/Specimen/Color/Color.js
+++ src/components/Specimen/Color/Color.js
@@ -3,8 +3,11 @@ import MetadataBlock from '../shared/MetadataBlock';
 import Radium from 'radium';
 
 class Color extends React.Component {
+  handleClick() {
+    document.execCommand('copy');
+  }
   render() {
-    const {theme, colors} = this.props;
+    const {theme, colors, modifiers} = this.props;
     let styles = {
       container: {
         display: 'flex',
@@ -12,19 +15,22 @@ class Color extends React.Component {
         flexWrap: 'wrap',
         justifyContent: 'flex-start',
         margin: '20px 0',
-        overflow: 'auto'
+        overflow: 'auto',
+        flexBasis: '100%'
+      },
+      rows: {
+        flexDirection: 'row'
+      },
+      columns: {
+        flexDirection: 'column'
       },
       box: {
         display: 'flex',
         flex: '0 1 auto',
         justifyContent: 'flex-start',
         flexDirection: 'row',
-        borderTop: `1px solid ${theme.lightColor}`,
         marginRight: theme.sizeL,
         padding: `${theme.sizeL / 2}px 0`
-      },
-      single: {
-        borderTop: 'none'
       },
       well: {
         alignSelf: 'flex-start',
@@ -32,7 +38,28 @@ class Color extends React.Component {
         height: '64px',
         marginRight: `${theme.sizeL / 2}px`,
         padding: `${theme.sizeL / 2}px`,
-        width: '64px'
+        width: '64px',
+        borderRadius: 64
+      },
+      mono: {
+        fontFamily: theme.fontMono,
+        cursor: 'copy'
+      },
+      paletteItem: {
+        padding: '20px',
+        flex: 1
+      },
+      paletteText: {
+        fontFamily: theme.fontFamily,
+        filter: 'invert(60%) hue-rotate(0deg)',
+        mixBlendMode: 'difference',
+        textOverflow: 'ellipsis',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        opacity: 0.5,
+        ':hover': {
+          opacity: 1
+        }
       },
       info: {
         alignSelf: 'flex-start',
@@ -41,12 +68,11 @@ class Color extends React.Component {
       }
     };
 
-    let singleElement = colors.length <= 1 ? styles.single : undefined;
-
+    let isPalette = modifiers.contains('palette') || modifiers.contains('palette-vertical');
 
     let colorSwatches = colors.map( (color, key) => {
       return (
-        <div key={'cg-Specimen-Color' + key} style={[styles.box, singleElement]}>
+        <div key={'cg-Specimen-Color' + key} style={styles.box}>
           <div style={{ ...styles.well, backgroundColor: color.value}}/>
           <div style={styles.info}>
             <MetadataBlock title={color.name} attributes={[color.value]} theme={theme}/>
@@ -54,9 +80,20 @@ class Color extends React.Component {
         </div>
       );
     });
+
+    let colorPalette = colors.map( (color, key) => {
+      return (
+        <div key={'cg-Specimen-Color' + key} style={{ ...styles.paletteItem,  backgroundColor: color.value}}>
+        <div style={{...styles.paletteText, color: color.value}} key={key}>
+          {color.name} <div style={styles.mono} onClick={this.handleClick}>{color.value}</div>
+        </div>
+        </div>
+      );
+    });
+
     return (
-      <section style={styles.container}>
-        {colorSwatches}
+      <section style={[styles.container, isPalette && !modifiers.contains('palette-vertical') ? styles.columns : styles.rows ]}>
+        {isPalette ? colorPalette : colorSwatches }
       </section>
     );
   }
@@ -64,6 +101,7 @@ class Color extends React.Component {
 
 Color.propTypes = {
   colors: PropTypes.array.isRequired,
+  modifiers: PropTypes.array,
   theme: PropTypes.object.isRequired
 };
 
