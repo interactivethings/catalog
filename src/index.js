5752aef51206d327b67cf21bdae43194601b79f2
Jeremy Stucki <jeremy@interactivethings.com>
2015-11-11 17:21:54 +0100

    Change export names

--- src/index.js
+++ src/index.js
@@ -8,17 +8,18 @@ export {default as configureRoutes} from './configureRoutes';
 // Components
 export {default as Card} from 'components/Card/Card';
 export {default as SpecimenWrapper} from 'components/Specimen/SpecimenWrapper';
+export {default as Span} from 'components/Specimen/Span';
 
 // Specimens
-export {default as Audio} from 'specimens/Audio';
-export {default as Button} from 'specimens/Button';
-export {default as Code} from 'specimens/Code';
-export {default as Color} from 'specimens/Color';
-export {default as Html} from 'specimens/Html';
-export {default as Hint} from 'specimens/Hint';
-export {default as Image} from 'specimens/Image';
-export {default as Type} from 'specimens/Type';
-export {default as Project} from 'specimens/Project/Project';
-export {default as Download} from 'specimens/Download';
-export {default as ReactComponent} from 'specimens/ReactComponent';
-export {default as Video} from 'specimens/Video';
+export {default as AudioSpecimen} from 'specimens/Audio';
+export {default as ButtonSpecimen} from 'specimens/Button';
+export {default as CodeSpecimen} from 'specimens/Code';
+export {default as ColorSpecimen} from 'specimens/Color';
+export {default as HtmlSpecimen} from 'specimens/Html';
+export {default as HintSpecimen} from 'specimens/Hint';
+export {default as ImageSpecimen} from 'specimens/Image';
+export {default as TypeSpecimen} from 'specimens/Type';
+export {default as ProjectSpecimen} from 'specimens/Project/Project';
+export {default as DownloadSpecimen} from 'specimens/Download';
+export {default as ReactSpecimen} from 'specimens/ReactComponent';
+export {default as VideoSpecimen} from 'specimens/Video';
