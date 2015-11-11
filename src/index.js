e1aaf2e2e6529d51549e400a8e12cc4239cfd0df
Jeremy Stucki <jeremy@interactivethings.com>
2015-11-11 17:10:52 +0100

    Export all specimens to library

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ export {default as configureRoutes} from './configureRoutes';
 
 // Components
 export {default as Card} from 'components/Card/Card';
+export {default as SpecimenWrapper} from 'components/Specimen/SpecimenWrapper';
 
 // Specimens
-export {default as ReactSpecimen} from 'specimens/ReactComponent';
+export {default as Audio} from 'specimens/Audio';
+export {default as Button} from 'specimens/Button';
+export {default as Code} from 'specimens/Code';
+export {default as Color} from 'specimens/Color';
+export {default as Html} from 'specimens/Html';
+export {default as Hint} from 'specimens/Hint';
+export {default as Image} from 'specimens/Image';
+export {default as Type} from 'specimens/Type';
+export {default as Project} from 'specimens/Project/Project';
+export {default as Download} from 'specimens/Download';
+export {default as ReactComponent} from 'specimens/ReactComponent';
+export {default as Video} from 'specimens/Video';
