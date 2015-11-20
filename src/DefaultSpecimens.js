ec32f206dcc3b2159b569cab02599e5755419d7a
Jeremy Stucki <jeremy@interactivethings.com>
2015-11-21 00:15:14 +0100

    Remove old UISpec, Icon, FramedCodeBlock specimens

--- src/DefaultSpecimens.js
+++ src/DefaultSpecimens.js
@@ -8,16 +8,6 @@ import Type from './specimens/Type';
 import Download from './specimens/Download';
 import Video from './specimens/Video';
 
-// Mostly replaced through Image/Video/Audio specimens
-// import UISpec from 'specimens/UISpec';
-
-// The used plugin is out of date at the moment
-// import FramedCodeBlock from 'specimens/FramedCodeBlock';
-
-// As is adds mor confusion than benefit at the moment, images and icons should
-// be displayed with either UISpec or Html for more complex needs
-// import Icon from 'specimens/Icon';
-
 export default {
   audio: Audio,
   code: Code,
