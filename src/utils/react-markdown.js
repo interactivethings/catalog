78fc544dfc5c90d8c2265618053ac795e12e7a01
Jeremy Stucki <jeremy@interactivethings.com>
2015-11-23 12:49:07 +0100

    Don't escape HTML entities for JSX

--- src/utils/react-markdown.js
+++ src/utils/react-markdown.js
@@ -361,25 +361,7 @@ ReactInlineLexer.prototype.output = function(src) {
 };
 
 function escape(html, encode) {
-  return html
-    .replace(!encode ? /&(?!#?\w+;)/g : /&/g, '&amp;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;');
-}
-
-function unescape(html) {
- return html.replace(/&([#\w]+);/g, function(_, n) {
-   n = n.toLowerCase();
-   if (n === 'colon') return ':';
-   if (n.charAt(0) === '#') {
-     return n.charAt(1) === 'x'
-       ? String.fromCharCode(parseInt(n.substring(2), 16))
-       : String.fromCharCode(+n.substring(1));
-   }
-   return '';
- });
+  return html;
 }
 
 
