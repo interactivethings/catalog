17e8d3bf8ce35d7efcc4a0cc13fe3f4e55f7b9db
Jeremy Stucki <jeremy@interactivethings.com>
2015-12-07 23:22:55 +0100

    Simplify markdown rendering

--- src/utils/MarkdownRenderer.js
+++ src/utils/MarkdownRenderer.js
@@ -1,4 +1,3 @@
-import R from 'ramda';
 import marked from './react-markdown';
 
 let MARKDOWN_CONFIG = {
@@ -9,35 +8,12 @@ let MARKDOWN_CONFIG = {
   smartypants: true
 };
 
-let splitIntoSections = (sections, node) => {
-  if (node.type === 'h2') {
-    sections.push([node]);
-  } else {
-    R.last(sections).push(node);
-  }
-  return sections;
-};
-
-let wrapSection = (sectionComponent) =>{
-  return (nodes, i) => {
-    if (i === 0) {
-      return nodes;
+module.exports = ({text, renderer}) => {
+  return marked(
+    text, 
+    {
+      ...MARKDOWN_CONFIG,
+      renderer: Object.assign(new marked.Renderer(), renderer)
     }
-    return sectionComponent(nodes);
-  };
-};
-
-module.exports = (options) => {
-  let renderer = new marked.Renderer();
-  let ref = options.renderer || {};
-  for (let type in ref) {
-    if ({}.hasOwnProperty.call(ref, type)) {
-      let handler = ref[type];
-      renderer[type] = handler;
-    }
-  }
-  let markedOptions = R.merge({
-    renderer: renderer
-  }, MARKDOWN_CONFIG);
-  return marked(options.text, markedOptions).reduce(splitIntoSections, [[]]).map(wrapSection(options.section));
+  );
 };
