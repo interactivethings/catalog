5e2b98b5ac656fcc70693de89f0e8f8cecf420a8
Jeremy Stucki <jeremy@interactivethings.com>
2015-12-08 09:54:31 +0100

    Improve nested lists

--- src/scaffold/lists.js
+++ src/scaffold/lists.js
@@ -3,28 +3,34 @@ import { inlineElements } from './typography';
 function baseListStyle() {
   return {
     width: '100%',
-    marginLeft: '2em',
-    paddingLeft: 0
+    marginLeft: 0,
+    paddingLeft: '2em'
   };
 }
 
-export function inlineUlist(theme, {selector, style = {}}) {
+export function inlineUlist(theme, {selector, style = {}, level = 0}) {
+  const nestedStyles = level < 3 ? inlineUlist(theme, {selector: `${selector} > li > ul`, style, level: level + 1}) : {};
   return {
     [selector]: {
+      listStyle: 'disc',
       ...baseListStyle(),
       ...style
     },
-    ...inlineElements(theme, {selector})
+    ...inlineElements(theme, {selector}),
+    ...nestedStyles,
   };
 }
 
-export function inlineOlist(theme, {selector, style = {}}) {
+export function inlineOlist(theme, {selector, style = {}, level = 0}) {
+  const nestedStyles = level < 3 ? inlineOlist(theme, {selector: `${selector} > li > ol`, style, level: level + 1}) : {};
   return {
     [selector]: {
+      listStyle: 'decimal',
       ...baseListStyle(),
       counterReset: 'item',
       ...style
     },
-    ...inlineElements(theme, {selector})
+    ...inlineElements(theme, {selector}),
+    ...nestedStyles,
   };
 }
