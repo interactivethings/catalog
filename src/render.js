f7b3dbc088b8c860bbd7eb8a34f384f7ec3a067f
Jeremy Stucki <jeremy@interactivethings.com>
2015-12-08 14:59:02 +0100

    Scroll to top on route changes

--- src/render.js
+++ src/render.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router} from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
+import useScroll from 'scroll-behavior/lib/useSimpleScroll';
 
 import configure from './configure';
 import configureRoutes from './configureRoutes';
 
 export default (config, element) => {
   ReactDOM.render(
-    <Router history={createHistory({queryKey: false})} routes={configureRoutes(configure(config))} />,
+    <Router history={useScroll(createHistory)({queryKey: false})} routes={configureRoutes(configure(config))} />,
     element
   );
 };
