1913eba90f489890f6a436a53f4ecd7e26b49e86
Jeremy Stucki <jeremy@interactivethings.com>
2016-01-01 12:10:18 +0100

    Add some basic configuration tests

---
+++ src/__tests__/configure-test.js
@@ -0,0 +1,133 @@
+import test from 'tape';
+import configure from '../configure';
+
+import DefaultTheme from '../DefaultTheme';
+import DefaultSpecimens from '../DefaultSpecimens';
+
+test('Configuration with default theme and specimens', (t) => {
+  t.deepEqual(configure({
+    title: 'Catalog',
+    pages: [
+      {
+        path: '/',
+        title: 'Overview',
+        src: 'overview.md'
+      }
+    ]
+  }), {
+    title: 'Catalog',
+    specimens: DefaultSpecimens,
+    theme: DefaultTheme,
+    pages: [
+      {
+        id: 1,
+        index: 0,
+        path: '/',
+        title: 'Overview',
+        superTitle: 'Catalog',
+        pages: null,
+        src: 'overview.md',
+        scripts: [],
+        styles: []
+      }
+    ],
+    pageTree: [
+      {
+        id: 1,
+        path: '/',
+        title: 'Overview',
+        superTitle: 'Catalog',
+        pages: null,
+        src: 'overview.md',
+        scripts: [],
+        styles: []
+      }
+    ]
+  });
+  t.end();
+});
+
+test('Configuration with nested pages', (t) => {
+  t.deepEqual(configure({
+    title: 'Catalog',
+    pages: [
+      {
+        path: '/',
+        title: 'Overview',
+        pages: [
+          {
+            path: 'foo',
+            title: 'Foo',
+            src: 'foo.md'
+          },
+          {
+            path: 'bar',
+            title: 'Bar',
+            src: 'bar.md'
+          }
+        ]
+      }
+    ]
+  }), {
+    title: 'Catalog',
+    specimens: DefaultSpecimens,
+    theme: DefaultTheme,
+    pages: [
+      {
+        id: 2,
+        index: 0,
+        path: '/foo',
+        title: 'Foo',
+        superTitle: 'Overview',
+        pages: null,
+        src: 'foo.md',
+        scripts: [],
+        styles: []
+      },
+      {
+        id: 3,
+        index: 1,
+        path: '/bar',
+        title: 'Bar',
+        superTitle: 'Overview',
+        pages: null,
+        src: 'bar.md',
+        scripts: [],
+        styles: []
+      }
+    ], // Why?
+    pageTree: [
+      {
+        id: 1,
+        path: '/',
+        title: 'Overview',
+        superTitle: 'Catalog',
+        pages: [
+          {
+            id: 2,
+            path: '/foo',
+            title: 'Foo',
+            superTitle: 'Overview',
+            pages: null,
+            src: 'foo.md',
+            scripts: [],
+            styles: []
+          },
+          {
+            id: 3,
+            path: '/bar',
+            title: 'Bar',
+            superTitle: 'Overview',
+            pages: null,
+            src: 'bar.md',
+            scripts: [],
+            styles: []
+          }
+        ],
+        scripts: [],
+        styles: []
+      }
+    ]
+  });
+  t.end();
+});
