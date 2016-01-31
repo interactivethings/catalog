f924e70bce49c2621f8b10bbae54ef4fcc3e0cd4
Jeremy Stucki <jeremy@interactivethings.com>
2016-01-31 21:16:50 +0100

    Allow `render` to be called more than once
    We need to supply a unique key to `<Router>`, so a new instance gets created because it doesn't allow replacement of routes.

--- src/render.js
+++ src/render.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import {Router} from 'react-router';
 import createHistory from 'history/lib/createHashHistory';
 import useScroll from 'scroll-behavior/lib/useSimpleScroll';
+import seqKey from './utils/seqKey';
 
 import configureRoutes from './configureRoutes';
 
+const history = useScroll(createHistory)({queryKey: false});
+
+const getKey = seqKey('CatalogRouter');
+
 export default (config, element) => {
   ReactDOM.render(
-    <Router history={useScroll(createHistory)({queryKey: false})} routes={configureRoutes(config)} />,
+    <Router key={getKey()} history={history} routes={configureRoutes(config)} />,
     element
   );
 };
