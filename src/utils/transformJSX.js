e7ee93c4434594e237b2aaa308c590bfa0e9c821
Jeremy Stucki <jeremy@interactivethings.com>
2016-02-07 00:31:14 +0100

    Remove logging

--- src/utils/transformJSX.js
+++ src/utils/transformJSX.js
@@ -17,17 +17,11 @@ export default (jsx, imports) => {
   try {
     const importKeys = Object.keys(imports);
     const importModules = importKeys.map((k) => imports[k]);
-    console.time('transform')
     const transformed = cachedTransform(jsx);
-    console.timeEnd('transform')
     const code = transformed.replace(/React\.createElement/, ';return React.createElement');
-    console.log(code)
-    console.time('render')
     const element = (new Function('React', ...importKeys, code))(React, ...importModules); // eslint-disable-line no-new-func
-    console.timeEnd('render')
     return {element};
   } catch (error) {
-    console.error('JSX transform failed:', error); // eslint-disable-line no-console
     return {error};
   }
 };
