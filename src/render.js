44a457a54758efde510c5e27663decbccff3fde3
Jeremy Stucki <jeremy@interactivethings.com>
2016-05-10 07:14:52 +0200

    Bump react-router, add as a dependency instead of peer dependency

--- src/render.js
+++ src/render.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, useRouterHistory} from 'react-router';
+import {Router, useRouterHistory, applyRouterMiddleware} from 'react-router';
 import {createHashHistory} from 'history';
-import useScroll from 'scroll-behavior/lib/useSimpleScroll';
+import useScroll from 'react-router-scroll';
 import seqKey from './utils/seqKey';
 
 import configureRoutes from './configureRoutes';
 
-const history = useRouterHistory(useScroll(createHashHistory))({queryKey: false});
+const history = useRouterHistory(createHashHistory)({queryKey: false});
 
 const getKey = seqKey('CatalogRouter');
 
 export default (config, element) => {
   ReactDOM.render(
-    <Router key={getKey()} history={history} routes={configureRoutes(config)} />,
+    <Router key={getKey()} history={history} routes={configureRoutes(config)} render={applyRouterMiddleware(useScroll())} />,
     element
   );
 };
