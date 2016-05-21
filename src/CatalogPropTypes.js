6a0611dcb7d182a4cfafebe5adbe3e9229b4099a
Jeremy Stucki <jeremy@interactivethings.com>
2016-05-21 22:14:46 +0200

    Ensure basePath in configuration

--- src/CatalogPropTypes.js
+++ src/CatalogPropTypes.js
@@ -9,19 +9,18 @@ export const pageShape = PropTypes.shape({
   pages: PropTypes.array, // should be arrayOf(page) but that doesn't work
   styles: PropTypes.array.isRequired,
   scripts: PropTypes.array.isRequired,
-  imports: PropTypes.object.isRequired,
-  basePath: PropTypes.string
+  imports: PropTypes.object.isRequired
 });
 
 export const pagesShape = PropTypes.arrayOf(pageShape);
 
 export const catalogShape = PropTypes.shape({
+  basePath: PropTypes.string.isRequired,
   page: pageShape.isRequired,
   getSpecimen: PropTypes.func.isRequired,
   theme: PropTypes.object.isRequired,
   title: PropTypes.string.isRequired,
   pages: pagesShape.isRequired,
   pageTree: pagesShape.isRequired,
-  logoSrc: PropTypes.string,
-  basePath: PropTypes.string
+  logoSrc: PropTypes.string
 });
