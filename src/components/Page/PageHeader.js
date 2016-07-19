84f2a533ddf7657a1ba61e602e9cf279124ca0ae
Tomas Carnecky <tomas.carnecky@gmail.com>
2016-07-19 21:33:37 +0200

    Move styles in PageHeader out of the JSX markup to make the DOM structure better visible (#161)

--- src/components/Page/PageHeader.js
+++ src/components/Page/PageHeader.js
@@ -6,36 +6,39 @@ class PageHeader extends Component {
   render() {
     const {theme, title, superTitle} = this.props;
 
-    return (
-      <div style={{
+    const styles = {
+      outerHeader: {
         boxSizing: 'border-box',
         position: 'relative',
         height: theme.pageHeadingHeight,
         background: theme.pageHeadingBackground
-      }} >
-        <div style={{
-          position: 'absolute',
-          bottom: theme.sizeL,
-          left: theme.sizeL,
-          '@media (min-width: 1000px)': {
-            left: theme.sizeL * 2
-          }
-        }} >
-          <h2 style={{
-            ...heading(theme, 1),
-            color: theme.pageHeadingTextColor,
-            opacity: 0.6,
-            margin: 0
-          }}>
-            {superTitle}
-          </h2>
-          <h1 style={{
-            ...heading(theme, 4),
-            color: theme.pageHeadingTextColor,
-            margin: 0
-          }}>
-            {title}
-          </h1>
+      },
+      innerHeader: {
+        position: 'absolute',
+        bottom: theme.sizeL,
+        left: theme.sizeL,
+        '@media (min-width: 1000px)': {
+          left: theme.sizeL * 2
+        }
+      },
+      superTitle: {
+        ...heading(theme, 1),
+        color: theme.pageHeadingTextColor,
+        opacity: 0.6,
+        margin: 0
+      },
+      title: {
+        ...heading(theme, 4),
+        color: theme.pageHeadingTextColor,
+        margin: 0
+      }
+    };
+
+    return (
+      <div style={styles.outerHeader}>
+        <div style={styles.innerHeader}>
+          <h2 style={styles.superTitle}>{superTitle}</h2>
+          <h1 style={styles.title}>{title}</h1>
         </div>
       </div>
     );
@@ -49,4 +52,3 @@ PageHeader.propTypes = {
 };
 
 export default Radium(PageHeader);
-
