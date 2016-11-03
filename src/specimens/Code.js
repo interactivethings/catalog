interactivethings/catalog

commit d49521bf6c6536cb296f04cf6545c0a9c95d1034

In-page links (#165)
Adds ability to link to page sections, and properly differentiates internal and external links.

Fixes #28 
Fixes #137

src/utils/path.js
@@ -1,0 +1,34 @@
+const removeMultiSlashes = (path) => path.replace(/\/+/g, '/');
+const stripTrailingSlashes = (path) => path.replace(/\/+$/, '');
+export const addLeadingSlash = (path) => path.charAt(0) === '/' ? path : '/' + path;
+const stripBasePath = (path, basePath) => basePath !== '/' && path.indexOf(basePath) === 0 ? path.substr(basePath.length) : path;
+
+export const parsePath = (path, options) => {
+  let pathname = stripBasePath(path, options.basePath);
+  let hash = '';
+  let anchor = null;
+
+  const hashIndex = pathname.indexOf('#');
+  if (hashIndex !== -1) {
+    hash = pathname.substr(hashIndex);
+    anchor = pathname.substr(hashIndex + 1);
+    pathname = pathname.substr(0, hashIndex);
+  }
+
+  if (pathname === '' && options.page) {
+    // fall back to current page path (already contains basePath)
+    pathname = stripBasePath(options.page.path, options.basePath);
+  }
+
+  // join basePath
+  pathname = addLeadingSlash(stripTrailingSlashes(removeMultiSlashes(options.basePath + '/' + pathname)));
+
+  return options.useBrowserHistory
+    ? {pathname, hash: hash === '#' ? '' : hash}
+    : {pathname, query: anchor ? {a: anchor} : {}};
+};
+
+export const isInternalPath = (path, options) => {
+  const {pathname} = parsePath(path, options);
+  return options.pagePaths.has(pathname);
+};

src/utils/path.test.js
@@ -1,0 +1,152 @@
+import {parsePath, isInternalPath} from './path';
+
+const mockCatalogConfig = {
+  basePath: '/',
+  useBrowserHistory: true,
+  pagePaths: new Set(['/', '/foo/bar']),
+  page: {
+    path: '/foo/bar'
+  }
+};
+
+test('Parse path', () => {
+  expect(parsePath('/foo/bar', mockCatalogConfig)).toEqual({pathname: '/foo/bar', hash: ''});
+});
+
+test('Parse path with trailing slash', () => {
+  expect(parsePath('/foo/bar/', mockCatalogConfig)).toEqual({pathname: '/foo/bar', hash: ''});
+});
+
+test('Parse path with hash', () => {
+  expect(parsePath('/foo/bar#baz', mockCatalogConfig)).toEqual({pathname: '/foo/bar', hash: '#baz'});
+});
+
+test('Parse path with only hash (pathname is current page)', () => {
+  expect(parsePath('#baz', mockCatalogConfig)).toEqual({pathname: '/foo/bar', hash: '#baz'});
+});
+
+test('Leave index path alone', () => {
+  expect(parsePath('/', mockCatalogConfig)).toEqual({pathname: '/', hash: ''});
+});
+
+const mockCatalogConfigWithHashHistory = {
+  basePath: '',
+  useBrowserHistory: false,
+  pagePaths: new Set(['/', '/foo/bar']),
+  page: {
+    path: '/foo/bar'
+  }
+};
+
+test('Hash history: Parse path', () => {
+  expect(parsePath('/foo/bar', mockCatalogConfigWithHashHistory)).toEqual({pathname: '/foo/bar', query: {}});
+});
+
+test('Hash history: Parse path with trailing slash', () => {
+  expect(parsePath('/foo/bar/', mockCatalogConfigWithHashHistory)).toEqual({pathname: '/foo/bar', query: {}});
+});
+
+test('Hash history: Parse path with hash', () => {
+  expect(parsePath('/foo/bar#baz', mockCatalogConfigWithHashHistory)).toEqual({pathname: '/foo/bar', query: {a: 'baz'}});
+});
+
+test('Hash history: Parse path with only hash (pathname is current page)', () => {
+  expect(parsePath('#baz', mockCatalogConfigWithHashHistory)).toEqual({pathname: '/foo/bar', query: {a: 'baz'}});
+});
+
+
+// Internal paths
+
+test('Internal path', () => {
+  expect(isInternalPath('/foo/bar', mockCatalogConfig)).toBe(true);
+});
+
+test('Internal path with trailing slash', () => {
+  expect(isInternalPath('/foo/bar/', mockCatalogConfig)).toBe(true);
+});
+
+test('Parse path with no leading slash', () => {
+  expect(parsePath('foo/bar', mockCatalogConfig)).toEqual({pathname: '/foo/bar', hash: ''});
+});
+
+test('Internal path with hash', () => {
+  expect(isInternalPath('/foo/bar#baz', mockCatalogConfig)).toBe(true);
+});
+
+test('Internal path with trailing slash and hash', () => {
+  expect(isInternalPath('/foo/bar/#baz', mockCatalogConfig)).toBe(true);
+});
+
+test('Internal path with only hash', () => {
+  expect(isInternalPath('#baz', mockCatalogConfig)).toBe(true);
+});
+
+test('External path', () => {
+  expect(isInternalPath('/whoa', mockCatalogConfig)).toBe(false);
+});
+
+
+// Base path
+
+const mockCatalogConfigWithBasePath = {
+  basePath: '/lalala',
+  useBrowserHistory: true,
+  pagePaths: new Set(['/lalala', '/lalala/foo/bar']),
+  page: {
+    path: '/lalala/foo/bar'
+  }
+};
+
+test('Parse path with basePath set', () => {
+  expect(parsePath('/foo/bar', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala/foo/bar', hash: ''});
+});
+
+test('Parse path with basePath set and basePath', () => {
+  expect(parsePath('/lalala/foo/bar', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala/foo/bar', hash: ''});
+});
+
+test('Parse path with basePath set and trailing slash', () => {
+  expect(parsePath('/foo/bar/', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala/foo/bar', hash: ''});
+});
+
+test('Parse path with basePath set and no leading slash', () => {
+  expect(parsePath('foo/bar', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala/foo/bar', hash: ''});
+});
+
+test('Parse path with basePath set and hash', () => {
+  expect(parsePath('/foo/bar#baz', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala/foo/bar', hash: '#baz'});
+});
+
+test('Parse path with basePath set and only hash (pathname is current page)', () => {
+  expect(parsePath('#baz', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala/foo/bar', hash: '#baz'});
+});
+
+// Internal paths
+
+test('Internal path basePath set', () => {
+  expect(isInternalPath('/foo/bar', mockCatalogConfigWithBasePath)).toBe(true);
+});
+
+test('Internal path with basePath set and trailing slash', () => {
+  expect(isInternalPath('/foo/bar/', mockCatalogConfigWithBasePath)).toBe(true);
+});
+
+test('Internal path with basePath set and hash', () => {
+  expect(isInternalPath('/foo/bar#baz', mockCatalogConfigWithBasePath)).toBe(true);
+});
+
+test('Internal path with basePath set and trailing slash and hash', () => {
+  expect(isInternalPath('/foo/bar/#baz', mockCatalogConfigWithBasePath)).toBe(true);
+});
+
+test('Internal path with basePath set and only hash', () => {
+  expect(isInternalPath('#baz', mockCatalogConfigWithBasePath)).toBe(true);
+});
+
+test('External path with basePath set', () => {
+  expect(isInternalPath('/whoa', mockCatalogConfigWithBasePath)).toBe(false);
+});
+
+test('Internal path basePath set and basePath', () => {
+  expect(isInternalPath('/lalala/foo/bar', mockCatalogConfigWithBasePath)).toBe(true);
+});

examples/catalog-module/index.js
@@ -5,6 +5,7 @@ import {Catalog} from '../../src/index';
 ReactDOM.render(
   <Catalog
     title='My Components'
+    useBrowserHistory={true}
     pages={[
       {
         path: '/',

src/CatalogPropTypes.js
@@ -23,5 +23,6 @@ export const catalogShape = PropTypes.shape({
   title: PropTypes.string.isRequired,
   pages: pagesShape.isRequired,
   pageTree: pagesShape.isRequired,
+  pagePaths: PropTypes.instanceOf(Set).isRequired,
   logoSrc: PropTypes.string
 });

src/__snapshots__/configure.test.js.snap
@@ -97,11 +97,11 @@ Object {
         "color": "#FF5555"
       }
     },
-    "fontFamily": "\'Roboto\', sans-serif",
-    "fontHeading": "\'Roboto\', sans-serif",
+    "fontFamily": "'Roboto', sans-serif",
+    "fontHeading": "'Roboto', sans-serif",
     "fontL": 21,
     "fontM": 16,
-    "fontMono": "\'Source Code Pro\', monospace",
+    "fontMono": "'Source Code Pro', monospace",
     "fontS": 14,
     "fontXl": 28,
     "fontXs": 12,
@@ -263,11 +263,11 @@ Object {
         "color": "#FF5555"
       }
     },
-    "fontFamily": "\'Roboto\', sans-serif",
-    "fontHeading": "\'Roboto\', sans-serif",
+    "fontFamily": "'Roboto', sans-serif",
+    "fontHeading": "'Roboto', sans-serif",
     "fontL": 21,
     "fontM": 16,
-    "fontMono": "\'Source Code Pro\', monospace",
+    "fontMono": "'Source Code Pro', monospace",
     "fontS": 14,
     "fontXl": 28,
     "fontXs": 12,

src/components/Catalog.js
@@ -22,12 +22,12 @@ export default class Catalog extends Component {
     });
   }
   render() {
-    const {useBrowserHistory, ...configuration} = this.props;
+    const configuration = this.props;
     const {routerKey} = this.state;
     return (
       <Router
         key={routerKey}
-        history={useBrowserHistory ? browserHistory : hashHistory}
+        history={configuration.useBrowserHistory ? browserHistory : hashHistory}
         routes={configureRoutes(configuration)}
         render={applyRouterMiddleware(useScroll())}
       />

src/components/CatalogContext.js
@@ -6,7 +6,7 @@ const fallbackPathRe = /\*$/;
 
 class CatalogContext extends Component {
   getChildContext() {
-    const {title, theme, logoSrc, pages, pageTree, specimens, basePath} = this.props.configuration;
+    const {title, theme, logoSrc, pages, pageTree, specimens, basePath, useBrowserHistory} = this.props.configuration;
     const {router} = this.context;
     return {
       catalog: {
@@ -15,9 +15,11 @@ class CatalogContext extends Component {
         theme,
         title,
         pages: pages.filter((p) => !p.hideFromMenu),
+        pagePaths: new Set(pages.map((p) => p.path)), // Used for internal link lookup
         pageTree,
         basePath,
-        logoSrc
+        logoSrc,
+        useBrowserHistory
       }
     };
   }

src/components/Link/HeadingLink.js
@@ -1,0 +1,38 @@
+import React, {PropTypes} from 'react';
+import Link from './Link';
+import {catalogShape} from '../../CatalogPropTypes';
+
+const style = (theme) => ({
+  headingLink: {
+    float: 'left',
+    marginLeft: -18,
+    color: theme.lightColor,
+    fill: theme.lightColor,
+    ':hover': {
+      color: theme.linkColor,
+      fill: theme.linkColor,
+      textDecoration: 'none'
+    }
+  }
+});
+
+const LinkIcon = () => (
+  <svg width='14px' height='14px' viewBox='0 0 16 16'><g  transform='translate(0, 0)'>
+  <path d='M4.5,16c-1.2,0-2.3-0.5-3.2-1.3c-1.8-1.8-1.8-4.6,0-6.4L2,7.6L3.4,9L2.7,9.7&#10;&#9;c-1,1-1,2.6,0,3.6c1,1,2.6,1,3.6,0l3-3c1-1,1-2.6,0-3.6L8.6,6L10,4.6l0.7,0.7c1.8,1.8,1.8,4.6,0,6.4l-3,3C6.9,15.5,5.7,16,4.5,16z'/>
+  <path d='M6,11.4l-0.7-0.7c-1.8-1.8-1.8-4.6,0-6.4l3-3c0.9-0.9,2-1.3,3.2-1.3s2.3,0.5,3.2,1.3c1.8,1.8,1.8,4.6,0,6.4&#10;&#9;L14,8.4L12.6,7l0.7-0.7c1-1,1-2.6,0-3.6c-1-1-2.6-1-3.6,0l-3,3c-1,1-1,2.6,0,3.6L7.4,10L6,11.4z'/>
+  </g></svg>
+);
+
+const HeadingLink = ({slug, ...rest}, {catalog}) => {
+  return <Link className='HeadingLink' title={'Link to this section'} to={`#${slug}`} aria-hidden style={style(catalog.theme).headingLink} {...rest}><LinkIcon /></Link>;
+};
+
+HeadingLink.propTypes = {
+  slug: PropTypes.string.isRequired
+};
+
+HeadingLink.contextTypes = {
+  catalog: catalogShape.isRequired
+};
+
+export default HeadingLink;

commit c3c19ebd9b0f100fdeed56f534fbfeee75f496c0

Fix index path when basePath is set (#251)
Fixes a regression introduced in #165.

src/specimens/Code.js
@@ -62,7 +62,7 @@ class Code extends React.Component {
         {toggle}
         {content}
       </section>
-      );
+    );
   }
 }
 

src/specimens/Hint.js
@@ -75,7 +75,7 @@ class Hint extends React.Component {
             </div>
           </section>
       </div>
-      );
+    );
   }
 }
 

src/specimens/Type.js
@@ -89,7 +89,7 @@ class Type extends React.Component {
             <div style={{...styles.title, ...fontColor}}>h{i + 1} ({heading + isPixel})</div>
             <div style={{...styles.heading, ...letterSpacing, font: `${isItalic} normal ${fontWeight} ${heading + isPixel} ${fontFamily}`}}>{headlineText}</div>
           </div>
-          );
+        );
       })
       : null;
 
@@ -107,7 +107,7 @@ class Type extends React.Component {
               {truncate ? `${dummyText.substring(0, 200)}…` : dummyText}
             </div>
           </div>
-          );
+        );
       })
       : null;
 
@@ -120,7 +120,7 @@ class Type extends React.Component {
             {description}
           </div>
         </section>
-      );
+    );
   }
 }
 

src/utils/path.js
@@ -4,7 +4,7 @@ export const addLeadingSlash = (path) => path.charAt(0) === '/' ? path : '/' + p
 const stripBasePath = (path, basePath) => basePath !== '/' && path.indexOf(basePath) === 0 ? path.substr(basePath.length) : path;
 
 export const parsePath = (path, options) => {
-  let pathname = stripBasePath(path, options.basePath);
+  let pathname = path;
   let hash = '';
   let anchor = null;
 
@@ -21,7 +21,8 @@ export const parsePath = (path, options) => {
   }
 
   // join basePath
-  pathname = addLeadingSlash(stripTrailingSlashes(removeMultiSlashes(options.basePath + '/' + pathname)));
+  pathname = addLeadingSlash(stripTrailingSlashes(removeMultiSlashes(options.basePath + '/' + stripBasePath(pathname, options.basePath))));
+
 
   return options.useBrowserHistory
     ? {pathname, hash: hash === '#' ? '' : hash}

src/utils/path.test.js
@@ -121,6 +121,10 @@ test('Parse path with basePath set and only hash (pathname is current page)', ()
   expect(parsePath('#baz', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala/foo/bar', hash: '#baz'});
 });
 
+test('Leave index path alone with basePath set', () => {
+  expect(parsePath('/lalala', mockCatalogConfigWithBasePath)).toEqual({pathname: '/lalala', hash: ''});
+});
+
 // Internal paths
 
 test('Internal path basePath set', () => {