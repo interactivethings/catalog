de060d95336e72c65a99be31379af035856fd0ed
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-03-08 11:58:39 +0100

    Describe where colors are used

--- src/DefaultTheme.js
+++ src/DefaultTheme.js
@@ -3,24 +3,38 @@
 export default {
   // Colors
   background: '#F9F9F9',
+  textColor: '#333333',
+  codeColor: '#00263E',
+  linkColor: '#FF5555',
+
+  // NavigationBar background color, but also sometimes used as a foreground
+  // or border color.
+  lightColor: '#D6D6D6',
+
+  // Used in PageHeader
   pageHeadingBackground: '#003B5C',
   pageHeadingTextColor: '#fff',
+
+  // Used in Menu and PageHeader to make sure the top parts have
+  // the same height.
   pageHeadingHeight: 200,
+
+  // NavigationBar (links), ResponsiveTabs (tab text), Download specimen (title text).
+  // Typography: headings.
   brandColor: '#003B5C',
+
   sidebarColor: '#FFFFFF',
   sidebarColorActive: '#D1312E',
   sidebarColorText: '#003B5C',
   sidebarColorTextActive: '#FF5555',
   sidebarColorLine: '#EBEBEB',
   sidebarColorHeading: '#003B5C',
-  textColor: '#333333',
-  codeColor: '#00263E',
-  linkColor: '#FF5555',
-  textMedium: '#003B5C',
+
+  // Used in the html, react, and image specimens.
   bgLight: '#F2F2F2',
   bgDark: '#333333',
-  lightColor: '#D6D6D6',
 
+  // Keys appear to be PrismJS token types.
   codeStyles: {
     tag: {color: '#FF5555'},
     punctuation: {color: '#535353'},
