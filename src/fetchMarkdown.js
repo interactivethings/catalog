c3e5e6666c2b0922830ed791304058620eb1d65a
Tomas Carnecky <tomas.carnecky@gmail.com>
2017-03-09 16:00:35 +0100

    Be more open what to accept as page content

--- src/fetchMarkdown.js
+++ src/fetchMarkdown.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import Page from './components/Page/Page';
 
-export default (url) =>
+const markdownContentTypes = [
+  'text/plain',
+  'text/x-markdown'
+];
+
+const isMarkdownContentType = (h) =>
+  markdownContentTypes.some(ct => h.includes(ct));
+
+const isMarkdownResponse = (res) =>
+  (res.status >= 200 && res.status < 300) &&
+  (!res.headers.has('content-type') || isMarkdownContentType(res.headers.get('content-type')));
+
+// Like 'fetchMarkdown' but doesn't catch errors in the promise.
+export const fetchMarkdownNoCatch = (url) =>
   fetch(url, {credentials: 'same-origin'})
     .then(res => {
-      if (res.status >= 200 && res.status < 300) {
+      if (isMarkdownResponse(res)) {
         return res.text();
       }
       throw new Error(`Unexpected status code: ${res.status}`);
-    })
-    .then(text => <Page>{text}</Page>)
-    .catch(error => <Page>{`Catalog.Content.fetchMarkdown: ${error}`}</Page>);
+    }).then(text => <Page>{text}</Page>);
+
+export default (url) =>
+  fetchMarkdownNoCatch(url)
+    .catch(error => <Page>{`Catalog.fetchMarkdown: ${error}`}</Page>);
