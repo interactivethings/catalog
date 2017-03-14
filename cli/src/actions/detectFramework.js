3819e431e89e03d6c4f28ffeb92cb02271ccac1c
Jeremy Stucki <jeremy@interactivethings.com>
2017-03-14 11:53:57 +0100

    Crude framework detection

--- cli/src/actions/detectFramework.js
+++ cli/src/actions/detectFramework.js
@@ -8,5 +8,19 @@ type NEXT = 'NEXT';
 type Framework = UNKNOWN | CREATE_REACT_APP | NEXT;
 
 export default async (paths: Object): Promise<Framework> => {
+  const appPackage = require(paths.appPackageJson);
+
+  if (appPackage.devDependencies.hasOwnProperty('react-scripts')) {
+    console.log('Detected Create React App');
+    return Promise.resolve('CREATE_REACT_APP');
+  }
+
+  if (appPackage.dependencies.hasOwnProperty('next')) {
+    console.log('Detected Next.js');
+    return Promise.resolve('NEXT');
+  }
+
+  console.log('No framework detected');
+
   return Promise.resolve('UNKNOWN');
 };
