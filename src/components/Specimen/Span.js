d3137d62235d097c2cc1a4162af6f371d3eeafe3
Gerhard Bliedung <gerhard@interactivethings.com>
2017-06-07 15:15:54 +0200

    refactor to stateless component, add flow types

--- src/components/Specimen/Span.js
+++ src/components/Specimen/Span.js
@@ -1,45 +1,35 @@
-import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+// @flow
+import React, {type Children} from 'react';
 import Radium from 'radium';
 
-class Span extends Component {
-  render() {
-    const {children, span} = this.props;
-
-    const style = {
-      boxSizing: 'border-box',
-      display: 'flex',
-      flexBasis: '100%',
-      // Bug fix for Firefox; width and flexBasis don't work on horizontally scrolling code blocks
-      maxWidth: '100%',
-      flexWrap: 'wrap',
-      margin: '24px 0 0 0',
-      padding: 0,
-      position: 'relative',
-      '@media (min-width: 640px)': {
-        flexBasis: `calc(${span / 6 * 100}% - 10px)`,
-        // Bug fix for Firefox; width and flexBasis don't work on horizontally scrolling code blocks
-        maxWidth: `calc(${span / 6 * 100}% - 10px)`,
-        margin: '24px 10px 0 0'
-      }
-    };
-
-    return (
-      <div {...this.props} style={{...style, ...this.props.style}}>
-        {children}
-      </div>
-    );
-  }
-}
-
-Span.propTypes = {
-  span: PropTypes.number,
-  children: PropTypes.node.isRequired,
-  style: PropTypes.object
+type SpanT = {
+  span: 1 | 2 | 3 | 4 | 5 | 6,
+  children: Children
 };
 
-Span.defaultProps = {
-  span: 6
+const Span = ({span = 6, children}: SpanT) => {
+  const style = {
+    boxSizing: 'border-box',
+    display: 'flex',
+    flexBasis: '100%',
+    // Bug fix for Firefox; width and flexBasis don't work on horizontally scrolling code blocks
+    maxWidth: '100%',
+    flexWrap: 'wrap',
+    margin: '24px 0 0 0',
+    padding: 0,
+    position: 'relative',
+    '@media (min-width: 640px)': {
+      flexBasis: `calc(${span / 6 * 100}% - 10px)`,
+      // Bug fix for Firefox; width and flexBasis don't work on horizontally scrolling code blocks
+      maxWidth: `calc(${span / 6 * 100}% - 10px)`,
+      margin: '24px 10px 0 0'
+    }
+  };
+  return (
+    <div style={style}>
+      {children}
+    </div>
+  );
 };
 
 export default Radium(Span);
