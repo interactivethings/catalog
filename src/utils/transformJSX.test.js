b7a90b1d5971eeeb41c64b676e93de7026d41791
Jeremy Stucki <jeremy@interactivethings.com>
2017-11-03 23:47:26 +0100

    Improve React Specimen code transform
    Instead of relying on a brittle heuristic detecting the first use of `React.createElement` *after* the code is already transformed, we now use a proper Babel plugin to return the last expression of the source code.
    
    Since this works with any expressions we can now also render arrays of React elements (or anything that React knows to render) when using React v16
    
    Fixes #328

--- src/utils/transformJSX.test.js
+++ src/utils/transformJSX.test.js
@@ -1,55 +1,41 @@
 import transformJSX from './transformJSX';
 
 test('Transforms a simple element', () => {
-  expect(transformJSX('<div>foo</div>', {}).code).toBe(
-    `"use strict";
-;return React.createElement(
-  "div",
-  null,
-  "foo"
-);`
-  );
+  expect(transformJSX('<div>foo</div>', {}).code).toMatchSnapshot();
 });
 
 test('Transforms a nested element', () => {
-  expect(transformJSX('<div><p>foo</p></div>', {}).code).toBe(
-    `"use strict";
-;return React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "p",
-    null,
-    "foo"
-  )
-);`
-  );
+  expect(transformJSX('<div><p>foo</p></div>', {}).code).toMatchSnapshot();
 });
 
 test('Transforms a component', () => {
-  expect(transformJSX('<Foo bar>baz</Foo>', {Foo: () => null}).code).toBe(
-    `"use strict";
-;return React.createElement(
-  Foo,
-  { bar: true },
-  "baz"
-);`
-  );
+  expect(transformJSX('<Foo bar>baz</Foo>', {Foo: () => null}).code).toMatchSnapshot();
 });
 
 test('Transforms a component which is defined inline', () => {
   expect(transformJSX(`const Foo = () => null;
-<Foo bar>baz</Foo>`, {}).code).toBe(
-    `"use strict";
+<Foo bar>baz</Foo>`, {}).code).toMatchSnapshot();
+});
 
-var Foo = function Foo() {
-  return null;
-};;return React.createElement(
-  Foo,
-  { bar: true },
-  "baz"
-);`
-  );
+test('Transforms an array of elements', () => {
+  expect(transformJSX(`[
+    <div key={1}>hey</div>,
+    <div key={2}>ho</div>
+  ]`, {}).code).toMatchSnapshot();
+});
+
+test('Returns the last expression', () => {
+  expect(transformJSX(`
+    React.createElement('div', {}, 'hey');
+    React.createElement('div', {}, 'ho');
+  `, {}).code).toMatchSnapshot();
+});
+
+test('Returns any expression', () => {
+  expect(transformJSX(`
+    const h = React.createElement;
+    h('div', {}, 'hey');
+  `, {}).code).toMatchSnapshot();
 });
 
 test('Returns error on syntax error', () => {
