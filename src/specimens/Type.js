c4e2c9572a6b3bb5be38bbd5a287eea5a04b4fb5
Matt Felten <m@mattfelten.com>
2017-11-21 11:18:29 -0800

    Make sure values aren't null

--- src/specimens/Type.js
+++ src/specimens/Type.js
@@ -84,8 +84,8 @@ class Type extends React.Component {
 
     const headings = options.headings
       ? options.headings.map( (heading, i) => {
-        const headingValue = typeof heading === 'object' ? heading.value : heading;
-        const headingLabel = typeof heading === 'object' ? heading.label : 'h' + (i + 1);
+        const headingValue = (heading !== null && typeof heading === 'object') ? heading.value : heading;
+        const headingLabel = (heading !== null && typeof heading === 'object') ? heading.label : 'h' + (i + 1);
         const isPixel = typeof headingValue === 'number' ? 'px' : '';
         return (
           <div key={i}>
@@ -98,8 +98,8 @@ class Type extends React.Component {
 
     const paragraphs = options.paragraphs
       ? options.paragraphs.map( (paragraph, i) => {
-        const paragraphValue = typeof paragraph === 'object' ? paragraph.value : paragraph;
-        const paragraphLabel = typeof paragraph === 'object' ? paragraph.label : 'Paragraph';
+        const paragraphValue = (paragraph !== null && typeof heading === 'object') ? paragraph.value : paragraph;
+        const paragraphLabel = (paragraph !== null && typeof heading === 'object') ? paragraph.label : 'Paragraph';
 
         const values = paragraphValue.split('/').map((item) => {
           return /[a-z]/i.test(item) ? `${item}` : `${item}px`;
