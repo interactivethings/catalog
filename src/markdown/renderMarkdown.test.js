interactivethings/catalog

commit ab6b0926e07310061f8777a9832f3746f59accc1

Fix Markdown links (#436)
Fixes #435

src/markdown/marked-react.js
@@ -166,7 +166,9 @@ ReactInlineLexer.prototype.output = function(src) {
     link,
     text,
     href,
-    cap;
+    title,
+    cap,
+    prevCapZero;
 
   while (src) {
     // escape
@@ -180,11 +182,8 @@ ReactInlineLexer.prototype.output = function(src) {
     if ((cap = this.rules.autolink.exec(src))) {
       src = src.substring(cap[0].length);
       if (cap[2] === "@") {
-        text =
-          cap[1].charAt(6) === ":"
-            ? this.mangle(cap[1].substring(7))
-            : this.mangle(cap[1]);
-        href = this.mangle("mailto:") + text;
+        text = escape(this.mangle(cap[1]));
+        href = "mailto:" + text;
       } else {
         text = escape(cap[1]);
         href = text;
@@ -195,9 +194,22 @@ ReactInlineLexer.prototype.output = function(src) {
 
     // url (gfm)
     if (!this.inLink && (cap = this.rules.url.exec(src))) {
+      do {
+        prevCapZero = cap[0];
+        cap[0] = this.rules._backpedal.exec(cap[0])[0];
+      } while (prevCapZero !== cap[0]);
       src = src.substring(cap[0].length);
-      text = escape(cap[1]);
-      href = text;
+      if (cap[2] === "@") {
+        text = escape(cap[0]);
+        href = "mailto:" + text;
+      } else {
+        text = escape(cap[0]);
+        if (cap[1] === "www.") {
+          href = "http://" + text;
+        } else {
+          href = text;
+        }
+      }
       out.push(this.renderer.link(href, null, text));
       continue;
     }
@@ -210,7 +222,13 @@ ReactInlineLexer.prototype.output = function(src) {
         this.inLink = false;
       }
       src = src.substring(cap[0].length);
-      out.push(this.options.sanitize ? escape(cap[0]) : cap[0]);
+      out.push(
+        this.options.sanitize
+          ? this.options.sanitizer
+            ? this.options.sanitizer(cap[0])
+            : escape(cap[0])
+          : cap[0]
+      );
       continue;
     }
 
@@ -218,10 +236,24 @@ ReactInlineLexer.prototype.output = function(src) {
     if ((cap = this.rules.link.exec(src))) {
       src = src.substring(cap[0].length);
       this.inLink = true;
+      href = cap[2];
+      if (this.options.pedantic) {
+        link = /^([^'"]*[^\s])\s+(['"])(.*)\2/.exec(href);
+
+        if (link) {
+          href = link[1];
+          title = link[3];
+        } else {
+          title = "";
+        }
+      } else {
+        title = cap[3] ? cap[3].slice(1, -1) : "";
+      }
+      href = href.trim().replace(/^<([\s\S]*)>$/, "$1");
       out.push(
         this.outputLink(cap, {
-          href: cap[2],
-          title: cap[3]
+          href: ReactInlineLexer.escapes(href),
+          title: ReactInlineLexer.escapes(title)
         })
       );
       this.inLink = false;

commit 15b810f60b4d48dbb530ffb7292b7ecb4c33d5ae

More markdown fixes (#437)
Fixes regressions introduced in #436

src/markdown/__snapshots__/renderMarkdown.test.js.snap
@@ -45,12 +45,42 @@ Array [
     <Styled.em>
       italic
     </Styled.em>
-     text.
+     text. Blank links 
+    <Unknown
+      title={null}
+      to="mailto:foo@bar.com"
+    >
+      foo@bar.com
+    </Unknown>
+     
+    <Unknown
+      title={null}
+      to="https://foobar.com"
+    >
+      https://foobar.com
+    </Unknown>
+  </Styled.p>,
+  "",
+  <pre>
+    <code
+      className={undefined}
+    >
+      a code block
+    </code>
+  </pre>,
+  <Styled.p>
+    Inline 
+    <Styled.code>
+      code
+    </Styled.code>
   </Styled.p>,
   "",
   <Styled.blockquote>
     <Styled.p>
-      Block quotes rock
+      Block quotes rock 
+      <Styled.em>
+        another em style
+      </Styled.em>
     </Styled.p>
   </Styled.blockquote>,
 ]
@@ -73,6 +103,15 @@ Array [
     HELLO LIST ITEM
     HELLO LIST ITEM
   </Styled.ul>,
+  "HELLO PARAGRAPH",
+  "",
+  <pre>
+    <code
+      className={undefined}
+    >
+      a code block
+    </code>
+  </pre>,
   "HELLO PARAGRAPH",
   "",
   <Styled.blockquote>

src/markdown/marked-react.js
@@ -2,46 +2,29 @@ import marked from "marked";
 
 /* eslint-disable */
 
-function ReactParser(options) {
-  this.tokens = [];
-  this.token = null;
-  this.options = options || {};
-  this.renderer = this.options.renderer;
-  this.renderer.options = this.options;
-}
+class ReactParser extends marked.Parser {
+  static parse(src, options) {
+    let parser = new ReactParser(options);
+    return parser.parse(src);
+  }
 
-ReactParser.parse = function(src, options) {
-  let parser = new ReactParser(options);
-  return parser.parse(src);
-};
-
-Object.assign(ReactParser.prototype, {
   parse(src) {
-    let out = [],
-      i = 0,
-      next;
-    this.inline = new ReactInlineLexer(src.links, this.options, this.renderer);
+    this.inline = new ReactInlineLexer(src.links, this.options);
+    // use an InlineLexer with a TextRenderer to extract pure text
+    this.inlineText = new marked.InlineLexer(
+      src.links,
+      Object.assign({}, this.options, { renderer: new marked.TextRenderer() })
+    );
     this.tokens = src.reverse();
 
+    let out = [];
     while (this.next()) {
       out.push(this.tok());
     }
 
-    return out; // React.DOM.div(null, out);
-  },
-  next() {
-    return (this.token = this.tokens.pop());
-  },
-  peek() {
-    return this.tokens[this.tokens.length - 1] || 0;
-  },
-  parseText() {
-    let body = this.token.text;
-    while (this.peek().type === "text") {
-      body += "\n" + this.next().text;
-    }
-    return this.inline.output(body);
-  },
+    return out;
+  }
+
   tok() {
     switch (this.token.type) {
       case "space": {
@@ -54,7 +37,7 @@ Object.assign(ReactParser.prototype, {
         return this.renderer.heading(
           this.inline.output(this.token.text),
           this.token.depth,
-          this.token.text
+          unescape(this.inlineText.output(this.token.text))
         );
       }
       case "code": {
@@ -70,13 +53,11 @@ Object.assign(ReactParser.prototype, {
           i,
           row,
           cell,
-          flags,
           j;
 
         // header
         cell = [];
         for (i = 0; i < this.token.header.length; i++) {
-          flags = { header: true, align: this.token.align[i] };
           cell.push(
             this.renderer.tablecell(this.inline.output(this.token.header[i]), {
               header: true,
@@ -104,7 +85,7 @@ Object.assign(ReactParser.prototype, {
         return this.renderer.table(header, body);
       }
       case "blockquote_start": {
-        var body = [];
+        body = [];
 
         while (this.next().type !== "blockquote_end") {
           body.push(this.tok());
@@ -113,17 +94,22 @@ Object.assign(ReactParser.prototype, {
         return this.renderer.blockquote(body);
       }
       case "list_start": {
-        var body = [],
-          ordered = this.token.ordered;
+        body = [];
+        var ordered = this.token.ordered,
+          start = this.token.start;
 
         while (this.next().type !== "list_end") {
           body.push(this.tok());
         }
 
-        return this.renderer.list(body, ordered);
+        return this.renderer.list(body, ordered, start);
       }
       case "list_item_start": {
-        var body = [];
+        body = [];
+
+        if (this.token.task) {
+          body.push(this.renderer.checkbox(this.token.checked));
+        }
 
         while (this.next().type !== "list_item_end") {
           body.push(this.token.type === "text" ? this.parseText() : this.tok());
@@ -132,7 +118,7 @@ Object.assign(ReactParser.prototype, {
         return this.renderer.listitem(body);
       }
       case "loose_item_start": {
-        var body = [];
+        body = [];
 
         while (this.next().type !== "list_item_end") {
           body.push(this.tok());
@@ -141,11 +127,8 @@ Object.assign(ReactParser.prototype, {
         return this.renderer.listitem(body);
       }
       case "html": {
-        let html =
-          !this.token.pre && !this.options.pedantic
-            ? this.inline.output(this.token.text)
-            : this.token.text;
-        return this.renderer.html(html);
+        // TODO parse inline content if parameter markdown=1
+        return this.renderer.html(this.token.text);
       }
       case "paragraph": {
         return this.renderer.paragraph(this.inline.output(this.token.text));
@@ -155,179 +138,187 @@ Object.assign(ReactParser.prototype, {
       }
     }
   }
-});
+}
 
-var ReactInlineLexer = marked.InlineLexer.prototype.constructor;
+class ReactInlineLexer extends marked.InlineLexer {
+  output(src) {
+    let out = [],
+      link,
+      text,
+      href,
+      title,
+      cap,
+      prevCapZero;
 
-ReactInlineLexer.prototype = Object.create(marked.InlineLexer.prototype);
-
-ReactInlineLexer.prototype.output = function(src) {
-  let out = [],
-    link,
-    text,
-    href,
-    title,
-    cap,
-    prevCapZero;
-
-  while (src) {
-    // escape
-    if ((cap = this.rules.escape.exec(src))) {
-      src = src.substring(cap[0].length);
-      out.push(cap[1]);
-      continue;
-    }
-
-    // autolink
-    if ((cap = this.rules.autolink.exec(src))) {
-      src = src.substring(cap[0].length);
-      if (cap[2] === "@") {
-        text = escape(this.mangle(cap[1]));
-        href = "mailto:" + text;
-      } else {
-        text = escape(cap[1]);
-        href = text;
-      }
-      out.push(this.renderer.link(href, null, text));
-      continue;
-    }
-
-    // url (gfm)
-    if (!this.inLink && (cap = this.rules.url.exec(src))) {
-      do {
-        prevCapZero = cap[0];
-        cap[0] = this.rules._backpedal.exec(cap[0])[0];
-      } while (prevCapZero !== cap[0]);
-      src = src.substring(cap[0].length);
-      if (cap[2] === "@") {
-        text = escape(cap[0]);
-        href = "mailto:" + text;
-      } else {
-        text = escape(cap[0]);
-        if (cap[1] === "www.") {
-          href = "http://" + text;
-        } else {
-          href = text;
-        }
-      }
-      out.push(this.renderer.link(href, null, text));
-      continue;
-    }
-
-    // tag
-    if ((cap = this.rules.tag.exec(src))) {
-      if (!this.inLink && /^<a /i.test(cap[0])) {
-        this.inLink = true;
-      } else if (this.inLink && /^<\/a>/i.test(cap[0])) {
-        this.inLink = false;
-      }
-      src = src.substring(cap[0].length);
-      out.push(
-        this.options.sanitize
-          ? this.options.sanitizer
-            ? this.options.sanitizer(cap[0])
-            : escape(cap[0])
-          : cap[0]
-      );
-      continue;
-    }
-
-    // link
-    if ((cap = this.rules.link.exec(src))) {
-      src = src.substring(cap[0].length);
-      this.inLink = true;
-      href = cap[2];
-      if (this.options.pedantic) {
-        link = /^([^'"]*[^\s])\s+(['"])(.*)\2/.exec(href);
-
-        if (link) {
-          href = link[1];
-          title = link[3];
-        } else {
-          title = "";
-        }
-      } else {
-        title = cap[3] ? cap[3].slice(1, -1) : "";
-      }
-      href = href.trim().replace(/^<([\s\S]*)>$/, "$1");
-      out.push(
-        this.outputLink(cap, {
-          href: ReactInlineLexer.escapes(href),
-          title: ReactInlineLexer.escapes(title)
-        })
-      );
-      this.inLink = false;
-      continue;
-    }
-
-    // reflink, nolink
-    if (
-      (cap = this.rules.reflink.exec(src)) ||
-      (cap = this.rules.nolink.exec(src))
-    ) {
-      src = src.substring(cap[0].length);
-      link = (cap[2] || cap[1]).replace(/\s+/g, " ");
-      link = this.links[link.toLowerCase()];
-      if (!link || !link.href) {
-        out.push(cap[0].charAt(0));
-        src = cap[0].substring(1) + src;
+    while (src) {
+      // escape
+      if ((cap = this.rules.escape.exec(src))) {
+        src = src.substring(cap[0].length);
+        out.push(cap[1]);
         continue;
       }
-      this.inLink = true;
-      out.push(this.outputLink(cap, link));
-      this.inLink = false;
-      continue;
+
+      // autolink
+      if ((cap = this.rules.autolink.exec(src))) {
+        src = src.substring(cap[0].length);
+        if (cap[2] === "@") {
+          text = escape(this.mangle(cap[1]));
+          href = "mailto:" + text;
+        } else {
+          text = escape(cap[1]);
+          href = text;
+        }
+        out.push(this.renderer.link(href, null, text));
+        continue;
+      }
+
+      // url (gfm)
+      if (!this.inLink && (cap = this.rules.url.exec(src))) {
+        do {
+          prevCapZero = cap[0];
+          cap[0] = this.rules._backpedal.exec(cap[0])[0];
+        } while (prevCapZero !== cap[0]);
+        src = src.substring(cap[0].length);
+        if (cap[2] === "@") {
+          text = escape(cap[0]);
+          href = "mailto:" + text;
+        } else {
+          text = escape(cap[0]);
+          if (cap[1] === "www.") {
+            href = "http://" + text;
+          } else {
+            href = text;
+          }
+        }
+        out.push(this.renderer.link(href, null, text));
+        continue;
+      }
+
+      // tag
+      if ((cap = this.rules.tag.exec(src))) {
+        if (!this.inLink && /^<a /i.test(cap[0])) {
+          this.inLink = true;
+        } else if (this.inLink && /^<\/a>/i.test(cap[0])) {
+          this.inLink = false;
+        }
+        src = src.substring(cap[0].length);
+        out.push(
+          this.options.sanitize
+            ? this.options.sanitizer
+              ? this.options.sanitizer(cap[0])
+              : escape(cap[0])
+            : cap[0]
+        );
+        continue;
+      }
+
+      // link
+      if ((cap = this.rules.link.exec(src))) {
+        src = src.substring(cap[0].length);
+        this.inLink = true;
+        href = cap[2];
+        if (this.options.pedantic) {
+          link = /^([^'"]*[^\s])\s+(['"])(.*)\2/.exec(href);
+
+          if (link) {
+            href = link[1];
+            title = link[3];
+          } else {
+            title = "";
+          }
+        } else {
+          title = cap[3] ? cap[3].slice(1, -1) : "";
+        }
+        href = href.trim().replace(/^<([\s\S]*)>$/, "$1");
+        out.push(
+          this.outputLink(cap, {
+            href: ReactInlineLexer.escapes(href),
+            title: ReactInlineLexer.escapes(title)
+          })
+        );
+        this.inLink = false;
+        continue;
+      }
+
+      // reflink, nolink
+      if (
+        (cap = this.rules.reflink.exec(src)) ||
+        (cap = this.rules.nolink.exec(src))
+      ) {
+        src = src.substring(cap[0].length);
+        link = (cap[2] || cap[1]).replace(/\s+/g, " ");
+        link = this.links[link.toLowerCase()];
+        if (!link || !link.href) {
+          out.push(cap[0].charAt(0));
+          src = cap[0].substring(1) + src;
+          continue;
+        }
+        this.inLink = true;
+        out.push(this.outputLink(cap, link));
+        this.inLink = false;
+        continue;
+      }
+
+      // strong
+      if ((cap = this.rules.strong.exec(src))) {
+        src = src.substring(cap[0].length);
+        out.push(
+          this.renderer.strong(
+            this.output(cap[4] || cap[3] || cap[2] || cap[1])
+          )
+        );
+        continue;
+      }
+
+      // em
+      if ((cap = this.rules.em.exec(src))) {
+        src = src.substring(cap[0].length);
+        out.push(
+          this.renderer.em(
+            this.output(
+              cap[6] || cap[5] || cap[4] || cap[3] || cap[2] || cap[1]
+            )
+          )
+        );
+        continue;
+      }
+
+      // code
+      if ((cap = this.rules.code.exec(src))) {
+        src = src.substring(cap[0].length);
+        out.push(this.renderer.codespan(escape(cap[2].trim(), true)));
+        continue;
+      }
+
+      // br
+      if ((cap = this.rules.br.exec(src))) {
+        src = src.substring(cap[0].length);
+        out.push(this.renderer.br());
+        continue;
+      }
+
+      // del (gfm)
+      if ((cap = this.rules.del.exec(src))) {
+        src = src.substring(cap[0].length);
+        out.push(this.renderer.del(this.output(cap[1])));
+        continue;
+      }
+
+      // text
+      if ((cap = this.rules.text.exec(src))) {
+        src = src.substring(cap[0].length);
+        out.push(escape(this.smartypants(cap[0])));
+        continue;
+      }
+
+      if (src) {
+        throw new Error("Infinite loop on byte: " + src.charCodeAt(0));
+      }
     }
 
-    // strong
-    if ((cap = this.rules.strong.exec(src))) {
-      src = src.substring(cap[0].length);
-      out.push(this.renderer.strong(this.output(cap[2] || cap[1])));
-      continue;
-    }
-
-    // em
-    if ((cap = this.rules.em.exec(src))) {
-      src = src.substring(cap[0].length);
-      out.push(this.renderer.em(this.output(cap[2] || cap[1])));
-      continue;
-    }
-
-    // code
-    if ((cap = this.rules.code.exec(src))) {
-      src = src.substring(cap[0].length);
-      out.push(this.renderer.codespan(escape(cap[2], true)));
-      continue;
-    }
-
-    // br
-    if ((cap = this.rules.br.exec(src))) {
-      src = src.substring(cap[0].length);
-      out.push(this.renderer.br());
-      continue;
-    }
-
-    // del (gfm)
-    if ((cap = this.rules.del.exec(src))) {
-      src = src.substring(cap[0].length);
-      out.push(this.renderer.del(this.output(cap[1])));
-      continue;
-    }
-
-    // text
-    if ((cap = this.rules.text.exec(src))) {
-      src = src.substring(cap[0].length);
-      out.push(escape(this.smartypants(cap[0])));
-      continue;
-    }
-
-    if (src) {
-      throw new Error("Infinite loop on byte: " + src.charCodeAt(0));
-    }
+    return out;
   }
-
-  return out;
-};
+}
 
 function escape(html, encode) {
   return html;

src/markdown/renderMarkdown.test.js
@@ -9,9 +9,15 @@ World
   - Two
 - Three
 
-A [link](http://www.interactivethings.com/) and some **bold** and *italic* text.
+A [link](http://www.interactivethings.com/) and some **bold** and *italic* text. Blank links foo@bar.com https://foobar.com
 
-> Block quotes rock
+~~~
+a code block
+~~~
+
+Inline \`code\`
+
+> Block quotes rock _another em style_
 `;
 
 test("Renders some Markdown", () => {