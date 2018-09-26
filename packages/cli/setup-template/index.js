50b8d5aa439b727b1b811480e51d51fa83037f07
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-09-26 08:09:21 +0200

    Trim @catalog/cli dependencies

--- packages/cli/setup-template/index.js
+++ packages/cli/setup-template/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Catalog, pageLoader } from "catalog";
+import { Catalog, pageLoader } from "@catalog/core";
 
 const pages = [
   {
