330fe92834fc021a5f3d4b5295b1aae65c9d9408
Tomas Carnecky <tomas.carnecky@gmail.com>
2018-10-09 22:20:10 +0200

    Inline strip-indent because jest doesn't agree how packages should be imported

--- packages/babel/src/reactspecimen-source.js
+++ packages/babel/src/reactspecimen-source.js
@@ -1,4 +1,16 @@
-import * as stripIndent from "strip-indent";
+// From package "strip-indent"
+const stripIndent = str => {
+  const match = str.match(/^[ \t]*(?=\S)/gm);
+
+  if (!match) {
+    return str;
+  }
+
+  const indent = Math.min(...match.map(x => x.length));
+  const re = new RegExp(`^[ \\t]{${indent}}`, "gm");
+
+  return indent > 0 ? str.replace(re, "") : str;
+};
 
 export default function({ types: t }) {
   return {
