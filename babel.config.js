6d90c0d2d98e17493c0f9b16a9ad17ed7e83e4cc
Jeremy Stucki <jeremy@interactivethings.com>
2019-02-05 15:55:34 +0100

    Fix tests

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,8 @@ module.exports = {
     test: {
       presets: [
         ["@babel/preset-env", { loose: true, targets: { node: "current" } }],
-        "@babel/preset-react"
+        "@babel/preset-react",
+        "@babel/preset-typescript"
       ],
       plugins: [
         "@babel/plugin-syntax-object-rest-spread",
