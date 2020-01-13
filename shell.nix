39179e10e095b76ed83ec9de16ed7a8894447ecf
Jeremy Stucki <jeremy@interactivethings.com>
2020-01-13 17:52:09 +0100

    fix: Add yarn to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,9 +1,11 @@
 let
-  pkgs = import <nixpkgs> {};
-  nodejs = pkgs.nodejs-10_x;
+  pkgs = import (fetchTarball https://nixos.org/channels/nixpkgs-unstable/nixexprs.tar.xz) { };
+  nodejs = pkgs.nodejs-12_x;
 
 in pkgs.mkShell {
   buildInputs = [
+    pkgs.darwin.apple_sdk.frameworks.CoreServices
+    pkgs.yarn
     nodejs
   ];
 }
